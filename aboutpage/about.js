d39414f1c07ad0af7ac6328cbc52320f68697964
Candice Barnsley <cebarnsley@gmail.com>
2019-03-27 13:56:46 +0000

    adding day night toggle button

---
+++ aboutpage/about.js
@@ -0,0 +1,8 @@
+function toggleMode() {
+    document.getElementById("body").classList.toggle("dayMode");
+    document.getElementById("heading-info").classList.toggle("info-day");
+    document.getElementById("p-info").classList.toggle("info-day");
+    document.getElementById("a-info").classList.toggle("info-day");
+    document.getElementById("button").classList.toggle("button-day");
+    
+}
---
+++ homepage/script.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+    
+    document.getElementById("body" ).classList.toggle("dayMode");
+    document.getElementById("p-main").classList.toggle("main-day");
+    document.getElementById("a-button").classList.toggle("button-day");
+    document.getElementById("button").classList.toggle("button-day");
+    
+    var fonts = document.getElementsByClassName("mainFont")
+    for (var i = 0; i < fonts.length; i++) {
+        fonts[i].classList.toggle("main-day")
+      }
+    
+}
